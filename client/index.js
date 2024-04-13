db2e49fce56a696db858da9edaf8bdc0668532ed
Gurvir Dhillon <gurvi@MacBook-Air.local>
2024-04-13 16:39:01 +0100

    added server need to work on the page ui and finalise design

--- client/index.js
+++ client/index.js
@@ -1,6 +1,36 @@
 document.addEventListener('DOMContentLoaded', () => {
   const startButton = document.querySelector('#start');
   startButton.addEventListener('click', () => {
-    prompt('hello');
+    // should open up the template and close this welcomes to lifes easier section
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  // Get references to elements
+  const startButton = document.getElementById('start');
+  const parentDiv = document.getElementById('parentDiv');
+  const getContextTemplate = document.getElementById('getContext');
+
+  startButton.addEventListener('click', function () {
+    const clonedTemplate = document.importNode(getContextTemplate.content, true);
+
+    const selectElement = clonedTemplate.querySelector('select');
+
+    const optionElements = selectElement.querySelectorAll('option');
+    optionElements.forEach((option, index) => {
+      if (index === 0) {
+        option.textContent = 'Education';
+      } else if (index === 1) {
+        option.textContent = 'Personal life';
+      } else if (index === 2) {
+        option.textContent = 'Business';
+      } else if (index === 3) {
+        option.textContent = 'Research/Science';
+      }
+    });
+
+    parentDiv.remove();
+
+    document.body.appendChild(clonedTemplate);
   });
 });
--- server/server.js
+++ server/server.js
@@ -1,5 +1,10 @@
 const express = require('express');
-
+const path = require('path');
+const app = express();
 const port = 8080;
 
+app.use(express.static((path.join('client'))));
 
+app.listen(port, function () {
+  console.log('server is available', port);
+});
